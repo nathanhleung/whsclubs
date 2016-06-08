4f7b07ac01318a13b3868808cf4ce83628bfa632
Nathan Leung <nathanhleung@users.noreply.github.com>
2016-06-07 22:18:53 -0400

    Update gradecalc.js

--- assets/js/gradecalc.js
+++ assets/js/gradecalc.js
@@ -51,11 +51,13 @@ angular.module('gradeCalcApp', ['ngRoute'])
         } else {
             $scope.needed = Math.round((($scope.desired - ($scope.average * $scope.multiplier)) / (1-$scope.multiplier)) * 100) / 100;
         }
+        $scope.shareUrl = 'https://whsclubs.herokuapp.com/gradecalculator#/?grades=' + $scope.grades.toString();
     }, true);
     // get grades from url if present
     if (typeof $location.search().grades !== 'undefined' && $location.search().grades !== '') {
       // grades query param is a list of comma-separated values
-      const urlGrades = $location.search().grades.split(',').map((grade) => {
+      // we have to add a comma to the beginning because the first value is removed otherwise for some reason
+      const urlGrades = (',' + $location.search().grades).split(',').map((grade) => {
         if (typeof grade !== 'undefined' && grade !== '') {
           return parseInt(grade, 10);
         } else {
